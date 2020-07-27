00bbe16ca1a6eb15d1079fbca7a0a271f4e490e2
cqtd <cqtd@protonmail.ch>
2020-07-28 04:45:17 +0900

    FPSCounter

---
+++ Assets/Scripts/UI/FPSCounter.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+
+namespace CQ.LeagueOfLegends.UI
+{
+	public class FPSCounter : MonoBehaviour
+	{
+		public TextMeshProUGUI fps;
+
+		float deltaTime = 0f;
+		bool fpsCounter = true;
+		
+		void Start()
+		{
+			StartCoroutine(RenewFPSCounterLoop());
+		}
+
+		void Update()
+		{
+			deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
+		}
+
+		void RenewFPSCounter()
+		{
+			fps?.SetText($"FPS:{1/deltaTime:N0}");
+		}
+
+		IEnumerator RenewFPSCounterLoop()
+		{
+			while (fpsCounter)
+			{
+				yield return new WaitForSeconds(1.0f);
+				RenewFPSCounter();		
+			}
+			
+			yield break;
+		}
+
+		void OnDestroy()
+		{
+			fpsCounter = false;
+		}
+	}
+}
