6be808400418214833a2b180c6f3b61a90396eb1
cqtd <cqtd@protonmail.ch>
2020-09-25 21:36:34 +0900

    .

---
+++ Assets/ShapeRenderer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class ShapeRenderer : VolumeComponent, IPostProcessComponent
+{
+
+
+    public bool IsActive()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool IsTileCompatible()
+    {
+        throw new System.NotImplementedException();
+    }
+}
